cfa603d06801a4d8f9b5b6142b46c1465f7c12fc
nitinshukla <nitin.shukla12004@gmail.com>
2024-11-11 16:54:55 +0530

    styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="grid grid-flow-col p-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1PwOypZPZT6YWFgtCZdokT9lKQ2-_IAagFQ&s"
+          alt="hamburger-logo"
+          className="h-12 mt-2"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcgDC5KbVCgEGLQgwB22LXxPNb4jBvyHwLmw&s"
+          alt="youtube-logo"
+          className="h-16"
+        />
+      </div>
+      <div className="col-span-8 flex items-center justify-center">
+        <input type="text" className="border-gray-400 border-2 w-1/2 rounded-full p-2" placeholder="Type to search"></input>
+        <button>Search</button>
+      </div>
+      <div className="col-span-3">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxMWBEVm_N2aPSbAbZrY2w5FBCPiv7zOuJ2A&s"
+          alt="user-logo" className="h-8"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
